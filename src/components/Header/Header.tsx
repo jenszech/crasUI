3509960c2c436211824869e1bf8221efc44bdf8d
Jens Zech <J.Zech@Thalia.de>
2020-03-07 21:16:20 +0100

    Navbar toogle changed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,52 @@
 import * as React from 'react';
-import {useState} from 'react';
 
 import {
     Navbar, Nav,
     NavbarBrand, NavLink, NavItem, Collapse, NavbarToggler
 } from 'reactstrap';
+import {Component} from "react";
 
 const AVATAR = './logo256.png';
 
-const Header = () => {
-    const [collapsed, setCollapsed] = useState(true);
+class Header extends Component {
+    state = {
+        collapsed: false
+    };
 
-    const toggleNavbar = () => setCollapsed(!collapsed);
+    private toggleNavbar = (): void => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    };
 
-    return (
-        <div>
-            <Navbar color="dark" dark expand="xs"
-                    style={{height: 80}}>
-                <img src={AVATAR} alt="crasUI" className="img-fluid rounded-circle" style={{width: 36, margin: 10}}/>
-                <NavbarBrand href="/" className="mr-auto">CRAS UI&nbsp;</NavbarBrand>
-                <NavbarToggler onClick={toggleNavbar} className="mr-2"/>
-                <Collapse isOpen={!collapsed} navbar>
-                    <Nav className="mrx-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/">Home</NavLink>
-                        </NavItem>
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="xs"
+                        style={{height: 80}}>
+                    <img src={AVATAR} alt="crasUI" className="img-fluid rounded-circle"
+                         style={{width: 36, margin: 10}}/>
+                    <NavbarBrand href="/" className="mr-auto">CRAS UI&nbsp;</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2"/>
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav className="mrx-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">Home</NavLink>
+                            </NavItem>
 
-                        <NavItem>
-                            <NavLink href="/overview">Raum</NavLink>
-                        </NavItem>
+                            <NavItem>
+                                <NavLink href="/overview">Raum</NavLink>
+                            </NavItem>
 
-                        <NavItem>
-                            <NavLink href="/detail">Termine</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
-    );
-};
+                            <NavItem>
+                                <NavLink href="/detail">Termine</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
 // noinspection JSUnusedGlobalSymbols
 export default Header;
