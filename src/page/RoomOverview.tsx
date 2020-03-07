d4d1957e79bad8229e6284c7f7d29e0696a0da65
Jens Zech <J.Zech@Thalia.de>
2020-03-07 01:24:15 +0100

    Dont work :-(

--- src/page/RoomOverview.tsx
+++ src/page/RoomOverview.tsx
@@ -2,23 +2,37 @@ import * as React from 'react';
 import {Col, Container, Row} from "reactstrap";
 import SideCard from "../components/SideCard";
 import TaskView from "../components/TaskView";
+import {Component} from "react";
+import {Room} from "../shared/models/Room";
+import { RouteComponentProps } from 'react-router';
 
-const roomOverview = () => {
-    return (
-        <Container>
-            <Row noGutters className="pt-md-3">
+class roomOverview extends Component<RouteComponentProps> {
+    constructor(props: Readonly<RouteComponentProps>) {
+        super(props);
+        console.log("constructor - roomOverview => "+ props.location.state);
+    }
 
-                <Col xs={{ order: 2 }} md={{ size: 3, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
-                    <SideCard />
-                </Col>
+    componentDidMount() {
+        console.log("componmentDidMount - roomOverview => "+ this);
+    }
 
-                <Col xs={{ order: 1 }} md={{ size: 7, offset: 1 }} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
-                    <TaskView />
-                </Col>
+    render() {
+        return (
+            <Container>
+                <Row noGutters className="pt-md-3">
 
-            </Row>
-        </Container>
-    );
-};
+                    <Col xs={{order: 2}} md={{size: 3, order: 1}} tag="aside"
+                         className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
+                        <SideCard/>
+                    </Col>
 
+                    <Col xs={{order: 1}} md={{size: 7, offset: 1}} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
+                        <TaskView/>
+                    </Col>
+
+                </Row>
+            </Container>
+        );
+    }
+}
 export default roomOverview;
